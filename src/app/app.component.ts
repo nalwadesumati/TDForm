f5208202e025a0ced1cee67e257331709e6ae8f4
mrpawar221@gmail.com <nalwadesumati@gmail.com>
2026-01-09 18:01:13 +0530

    TDForm using ng material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'TDFORM';
+  @ViewChild('signUpForm') signUpForm!: NgForm;
+  userDetails = {
+    username: 'suammati',
+    email: 'sumati@gmai.com',
+    secretQuestion: 'favColor',
+    answer: 'yellow',
+    modeOfContact: 'phone',
+  };
+  isEditMode = false;
+  secretQuestion!: string;
+  secretAnswer!: string;
+  isSubscribe!: boolean;
+  modeOfContact: any;
+  ngOnInit(): void {
+    // setTimeout(() => {
+    //   console.log(this.signUpForm);
+    // }, 0);
+  }
+  onSubmit() {
+    if (this.signUpForm.valid) {
+      console.log(this.signUpForm.value);
+      this.signUpForm.reset();
+    }
+  }
+  patchData() {
+    this.signUpForm.form.patchValue(this.userDetails);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatRadioModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
